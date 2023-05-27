404ea99d13ad410dece26fc8dc2bb5793189a58a
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-27 22:22:39 +1000

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), prefetch()]
 });
